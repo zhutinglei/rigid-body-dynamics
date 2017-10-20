8e56e7ae868d8d46adb66a7ac0192761ab36ca8e
zhutinglei <zhutinglei@gmail.com>
2017-10-20 16:58:33 +0800

    finish Pn, Pb and Pt

--- rigid-body-dynamics/differential_area.py
+++ rigid-body-dynamics/differential_area.py
@@ -1,6 +1,7 @@
 #!/usr/local/bin/python3
 
 import numpy as np
+import scipy.special.erf as erf
 
 class DifferentialArea:
     """
@@ -8,9 +9,9 @@ class DifferentialArea:
     be unit vector
 
     Parameters:
-    ==========
+    ===========
     position, angular_velocity: 3-d vector (double)
-    normal, tangent, binormal: 3-d unit vector (double), local frame
+    normal (inner), binormal, tangent: 3-d unit vector (double), local frame
 
     """
     def __init__(self, position=None, angular_velocity=None, normal=None):
@@ -23,15 +24,100 @@ class DifferentialArea:
         else:
             self.angular_velocity = angular_velocity
         if normal is None:
-            self.normal = self.position
+            self.normal = -self.position/np.linalg.norm(self.position)
         else:
             self.normal = normal/np.linalg.norm(normal)
         
-        self.tangent = np.cross(angular_velocity, position)
-        assert(self.tangent != np.zeros([3])
-        self.tangent /= np.linalg.norm(self.tangent)
-        self.binormal = np.cross(self.normal, self.tangent)
+        self.velocity = np.cross(angular_velocity, position)
+        assert(self.velocity != np.zeros([3])
+        self.binormal = np.cross(self.tangent, self.normal)
+        self.tangent = self.velocity / np.linalg.norm(self.velocity)
 
+    def maxwellian_vdf(self, flow, velocity):
+        """
+        Maxwellian velocity distribution function of the molecules w.r.r to the
+        differential area.
+
+        Parameters:
+        ===========
+        flow: `~FreeMolecularFlow`
+        velocity: 3-d vector, m/s 
+
+        Return:
+        =======
+        vdf: double
+        """
+        gc = flow.GASCONST
+        t = flow.temperature
+        v = flow.velocity - self.velocity
+        vdf = (2.0*np.pi*gc*t)-1.5)*np.exp(
+            -0.5/gc/t*np.dot(velocity-v, velocity-v)
+        )
+        return vdf
+
+    def pressure(self, flow, wall_temperature=None, sigma=None):
+        """
+        Pressure Pn and tangential stresses Pb, Pt.
+
+        Parameter:
+        ==========
+        flow: `~FreeMolecularFlow`
+
+        Return:
+        =======
+        [Pni, Pbi, Pti]: Ndarray, size 3
+        """
+        tw = 300.0 if wall_temperature == None else wall_temperature
+        if sigma == None:
+            sigma_t = 1.0
+            sigma_n = 1.0
+        else:
+            assert(len(sigma)==2)
+            sigma_n, sigma_t = sigma
+
+        scale = np.sqrt(2.0*flow.GASCONST*flow.temperature)
+        s_flow = flow.velocity / scale
+        s_area = s_flow - self.velocity / scale
+        w_area = self.angular_velocity / scale
+        a = flow.density/2.0/np.sqrt(np.pi)*scale**2.0
+        
+        frame = np.matrix([self.normal, self.binormal, self.tangent]).transpose()
+        sn, sb, st = np.array(np.dot(s_area, frame)).flatten()
+
+        def _chi(x):
+            return np.exp(-x**2) + x * np.sqrt(np.pi) * (1.0+erf(x))
+        def _xi(x):
+            return _chi(x)*x + 0.5 * np.sqrt(np.pi) * (1.0+erf(x))
+        
+        Pni = a * _xi(sn)
+        Pbi = a * sb * _chi(sn)
+        Pti = a * st * _chi(sn)
+        Pw = a * np.sqrt(np.pi)/2.0*np.sqrt(tw/flow.temperature)*_chi(sn)
+        
+        Pn = (2.0 - sigma_n) * Pni + sigma_n * Pw
+        Pb = sigma_t * Pbi
+        Pt = sigma_t * Pti
+
+        return np.array([Pn, Pb, Pt])
+
+class FreeMolecularFlow:
+    """
+    Define a free molecular flow
+
+    Parameters:
+    ===========
+    GASCONST: const, kg m^2 / (s^2 K mol)
+    temperature: kelvins, double
+    density: kg / m^3, double
+    velocity: m/s, 3-d vector, double
+
+    """
+    GASCONST = 8.3144598 
+    def __init__(self, temperature=None, density=None, velocity=None):
+        self.temperature = 1000.0 if temperature == None else temperature
+        self.density = 1.0e-12 if density == None else density
+        self.velocity = 7500.0 if velocity == None else velocity
+            
 
 def test():
     diff_area = DifferentialArea()
