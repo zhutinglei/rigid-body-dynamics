7d2c21601c2f6e35c80222e751977ab8f5e4bfa4
zhutinglei <zhutinglei@Tinglei-MacBook-Pro.local>
2017-10-22 15:53:09 +0800

    modify rigid_body.py, some TODOs left

--- rigid-body-dynamics/rigid_body.py
+++ rigid-body-dynamics/rigid_body.py
@@ -4,7 +4,6 @@ import numpy as np
 
 MINDBL = np.finfo(np.double).tiny
 
-
 def _ismatrix(mat, shape=None):
     if isinstance(mat, np.matrix):
         return True if shape is None else (mat.shape == shape)
@@ -70,6 +69,7 @@ class RigidBody:
 
 # Private: 
 
+    __angle_types = ('zxz', 'xyz')
     __mass = 1000.0
     __inertia = np.matrix(np.identity(3))
     
@@ -95,6 +95,11 @@ class RigidBody:
     def inertia(self):
         return __inertia
 
+    def get_euler_angles(self, angle_type):
+        assert(angle_type in __angle_types)
+        ### TODO
+        pass
+
     ### Methods
     def set_state(self, attitude=None, angular_vel=None):
         if attitude is not None:
@@ -103,7 +108,27 @@ class RigidBody:
         if angular_vel is not None:
             assert(np.size(angular_vel) == 3)
             self.angular_vel = np.matrix(angular_vel).reshape([3,1])
+    
+    def set_attitude_from_angles(self, angles, angle_type):
+        assert(angle_type in __angle_types)
+        # TODO
+        pass
+
+    def free_propagate(self, attitude=None, angular_vel=None, time_delta=None):
+        """
+            Propagate rigid body in Euler case, i.e. no external torque
+
+        Parameters:
+        ==========
 
 
+        Return:
+        =======
+        """
+        self.set_state(attitude, angular_vel)
+        if time_delta is None:
+            return self
+        # TODO
+        pass
 
 
