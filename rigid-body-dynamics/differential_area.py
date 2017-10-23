b2a93a7ec81f039686c54987423ff39ca0472d1a
zhutinglei <zhutinglei@gmail.com>
2017-10-23 14:45:53 +0800

    modify differential_area.py

--- rigid-body-dynamics/differential_area.py
+++ rigid-body-dynamics/differential_area.py
@@ -8,30 +8,30 @@ import scipy.constants as const
 
 class DifferentialArea:
     """
-    Require position, normal, angular_velocity be 3-d vector, and normal should 
+    Require position, normal, angular_vel be 3-d vector, and normal should 
     be unit vector
 
     Parameters:
     ===========
-    position, angular_velocity: 3-d vector (double)
+    position, angular_vel: 3-d vector (double)
     normal (inner), binormal, tangent: 3-d unit vector (double), local frame
 
     """
-    def __init__(self, position=None, angular_velocity=None, normal=None):
+    def __init__(self, position=None, angular_vel=None, normal=None):
 	    if position is None:
             self.position = np.array([-1.0, 0.0, 0.0])
         else: 
             self.position = position
-        if angular_velocity is None:
-            self.angular_velocity = np.array([0.0, 0.0, 1.0])
+        if angular_vel is None:
+            self.angular_vel = np.array([0.0, 0.0, 1.0])
         else:
-            self.angular_velocity = angular_velocity
+            self.angular_vel = angular_vel
         if normal is None:
             self.normal = -self.position/np.linalg.norm(self.position)
         else:
             self.normal = normal/np.linalg.norm(normal)
         
-        self.velocity = np.cross(angular_velocity, position)
+        self.velocity = np.cross(angular_vel, position)
         assert(self.velocity != np.zeros([3])
         self.binormal = np.cross(self.tangent, self.normal)
         self.tangent = self.velocity / np.linalg.norm(self.velocity)
@@ -52,7 +52,7 @@ class DifferentialArea:
         """
         t = flow.temperature
         v = flow.velocity - self.velocity
-        vdf = (2.0*np.pi*const.R*t)-1.5)*np.exp(
+        vdf = (2.0*np.pi*const.R*t)**(-1.5)*np.exp(
             -0.5/const.R/t*np.dot(velocity-v, velocity-v)
         )
         return vdf
@@ -64,15 +64,18 @@ class DifferentialArea:
         Parameter:
         ==========
         flow: `~FreeMolecularFlow`
+        wall_temperature: optional, float, unit = Kelvin
+        sigma: optional, array-like, len(sigma)=2
+            Normal accommodation parameter sigma_n, and tangent 
+        accommodation parameter sigma_t
 
         Return:
         =======
         [Pni, Pbi, Pti]: Ndarray, size 3
         """
-        tw = 300.0 if wall_temperature == None else wall_temperature
-        if sigma == None:
-            sigma_t = 1.0
-            sigma_n = 1.0
+        tw = 300.0 if wall_temperature is None else wall_temperature
+        if sigma is None:
+            sigma_t, sigma_n = 1.0, 1.0
         else:
             assert(len(sigma)==2)
             sigma_n, sigma_t = sigma
@@ -80,7 +83,7 @@ class DifferentialArea:
         scale = np.sqrt(2.0*const.R*flow.temperature)
         s_flow = flow.velocity / scale
         s_area = s_flow - self.velocity / scale
-        w_area = self.angular_velocity / scale
+        w_area = self.angular_vel / scale
         a = flow.density/2.0/np.sqrt(np.pi)*scale**2.0
         
         frame = np.matrix([self.normal, self.binormal, self.tangent]).transpose()
