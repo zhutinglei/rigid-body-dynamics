159ed823948315fa92a664865d4689edc6f4c4c7
zhutinglei <zhutinglei@gmail.com>
2017-10-23 17:11:53 +0800

    rigid body

--- rigid-body-dynamics/rigid_body.py
+++ rigid-body-dynamics/rigid_body.py
@@ -6,13 +6,14 @@ import checks as ck
 
 class RigidBody:
     """
-        Define the status of a rigid body, the center of mass is fixed 
-    as origin.
+    Define the status of a rigid body, the center of mass is fixed as 
+    origin.
 
     Parameters:
     ==========
     mass: float 
         unit = kg, default = 1000.0 
+
     inertia: `numpy.matrix`, shape=[3,3] 
         unit = kg*m^2, default = np.matrix(np.identity), inertia is a 
     symmetric matrix represented in the body-fixed frame (BFF), so it is
@@ -21,8 +22,9 @@ class RigidBody:
     attitude: `numpy.matrix`, shape=[3,3] 
         attitude is an orthongonal matrix, i.e. inverse = transpose, the
     columns are coordinates of the axes of BFF in translation frame (TF)
-    angular_vel: `numpy.matrix`, shape=[3,1] angular velocity of the 
-    rigid body, given in BFF. 
+
+    angular_vel: `numpy.matrix`, shape=[1,3]
+        angular velocity of the rigid body, given in BFF. 
     
     """
 
@@ -30,7 +32,7 @@ class RigidBody:
 
     __angle_types = ('zxz', 'xyz')
     __mass = 1000.0
-    __inertia = np.matrix(np.identity(3))
+    __inertia = np.identity(3)
     
     def __init__(self, mass=None, inertia=None):
         if mass is not None:
@@ -38,12 +40,13 @@ class RigidBody:
             self.mass = mass
         if inertia is not None:
             assert(ck.issymmetric(inertia, 3))
+
 # Public:
 
     attitude = np.matrix(np.identity(3))
     angular_vel = np.matrix(np.zeros(3)).transpose()
     
-    ### Attributes
+    ''' Attributes '''
     def mass(self):
         return __mass
 
@@ -55,7 +58,36 @@ class RigidBody:
         ### TODO 
         pass
 
-    ### Methods
+    def changing_rate(self, torque=None):
+        """
+        Changint rate of attitude and angular_vel due to the Euler 
+        dynamic and kinetic equations
+        
+        Parameters:
+        ===========
+        torque: optional, np.matrix, shape = [3,1]
+            The external torque on the rigid body, represented in BFF.
+
+        Return:
+        =======
+        dw: np.matrix, shape = [3,1]
+            Changing rate of angular velocity, represented in BFF.
+        da: np.matrix, shpae = [3,3] 
+            Changing rate of attitude matrix, represented in TF i.e.
+        the Kronig frame.
+
+        """
+        if torque is None:
+            torque = np.matrix(np.zeros([3,1]))
+        else:
+            assert(ismatrix(torque,[3,1]))
+
+        j, w, a = self.__inertia, self.angular_vel, self.attitude
+        w_crs = np.cross(np.eye(3), w.getA1())
+        dw = np.linalg.inv(j) * (torque - w_crs * J * w)
+        da = a * w_crs * a
+
+    ''' Methods '''
     def set_state(self, attitude=None, angular_vel=None):
         if attitude is not None:
             assert(ck.isorthogonal(attitude, 3))
@@ -68,6 +100,7 @@ class RigidBody:
         assert(angle_type in __angle_types)
         # TODO
         pass
+
 
     def free_propagate(self, attitude=None, angular_vel=None, time_delta=None):
         """
